ebdbc21cd397a099dcaec8498cbc9c1b2a25e223
cunshanggangg <cunshanggangg@gmail.com>
2017-03-20 17:55:05 +0800

    How to use getdate().

---
+++ time/getDate.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/3/20
+ * Time: 12:01
+ */
+
+$time = getdate("1489311032");
+echo "<pre>";
+print_r($time);
+echo "</pre>";
