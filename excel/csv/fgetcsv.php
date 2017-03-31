f87a7bf53fc171e9dc78edb750beea4d6a677631
cunshanggangg <cunshanggangg@gmail.com>
2017-03-31 15:06:49 +0800

    Page garbled.

--- excel/csv/fgetcsv.php
+++ excel/csv/fgetcsv.php
@@ -24,7 +24,7 @@ if (($handle = fopen("test.csv", "r")) !== FALSE) {
     fclose($handle);
 }
 */
-
+//header("Content-type:text/html;charset=gb2312");
 //按照数组的形式打印出来
 $file = fopen("test.csv","r");
 
@@ -35,8 +35,13 @@ while(! feof($file))
 //    print_r(fgetcsv($file));
 //    print_r($res);
     $res = fgetcsv($file);
-    foreach((array)$res as $key => $value){
+//    echo "<pre>";
+//    print_r($res);
+//    echo "</pre>";
+    foreach((array)$res as $key => $value){//声明该变量为数组
+        echo $key;
         echo iconv("gb2312","UTF-8",$value);
+        echo "<br />";
     }
     //echo "</pre>";
 }
