48bb693d7750eb0c30c85a24a4153f880810f820
cunshanggangg <cunshanggangg@gmail.com>
2017-04-17 15:00:22 +0800

    The difference between array_slice() and array_splice().

--- array/array_slice.php
+++ array/array_slice.php
@@ -16,3 +16,30 @@ $arr = array(
 echo "<pre>";
 print_r(array_slice($arr,1,3,true));//true显示原来的索引值
 echo "</pre>";
+
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
+
+echo "与array_splice()的区别\n";
+$arr1 = array(
+    "0"=>"a",
+    "1"=>"b",
+    "2"=>"c",
+    "3"=>"d"
+);
+echo "<pre>";
+print_r(array_splice($arr1,1,3));
+echo "</pre>";
+
+echo "<pre>";
+print_r($arr1);
+echo "</pre>";
+
+/* 与array_splice()区别：
+ *
+ * array_splice()：分割后数组分成两部分
+ *
+ * array_slice():获取到分割部分，但原数组仍然保持原来的数据
+ *
+ * */
