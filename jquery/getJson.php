77ab5ed6f155cdd0ea7ad95d63737862bd02a6dc
cunshanggangg <cunshanggangg@gmail.com>
2017-04-22 10:38:13 +0800

    Using $.getJson function to sent json data.

--- jquery/getJson.php
+++ jquery/getJson.php
@@ -6,10 +6,19 @@
  * Time: 16:52
  */
 if($_REQUEST['act'] == "getInfo"){
-//    echo "yaoMing";
-    $re['userName'] = $_GET['userName'];
+////    echo "yaoMing";
+    $re['userName'] = $_GET['username'];
     $re['password'] = $_GET['password'];
 
-    echo "dddddddddd";
-    return json_encode($re);
+    jsonStr($re);
+//    echo json_encode($re);
+//    echo "<pre>";
+//    print_r(json_encode($re));
+//    echo "</pre>";
+    //exit(json_encode($re));
+}
+
+function jsonStr($data) {
+//    return json_encode($data);
+    exit(json_encode($data));
 }
