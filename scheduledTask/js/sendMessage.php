e3a437eb08f8b8d1cc924ef0e820925a68df4bda
cunshanggangg <cunshanggangg@gmail.com>
2017-05-03 16:51:10 +0800

    php js ajax

--- scheduledTask/js/sendMessage.php
+++ scheduledTask/js/sendMessage.php
@@ -5,6 +5,29 @@
  * Date: 2017/5/2
  * Time: 17:33
  */
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+//echo "<pre>";
+//print_r($_POST);
+//echo "</pre>";
+//exit($_POST);
+
+//$sum = $_POST['num']+10;
+//echo $sum;
+//exit($sum);
+//return $sum;
+//echo $_POST['num'];
+//echo $_POST['name'];
+//echo $_POST['num']+10;
+//$arr = array("linn","dddddddddd");
+//exit(print_r($arr));
+
+//if($_POST['num'] == '') {
+
+//}
+if($_POST){
+    $_POST['num'] = $_POST['num']+10;
+}else{
+    $_POST['num'] = 158;
+}
+exit(json_encode($_POST));
+//exit(print_r($_POST));
+//print_r($_POST);
