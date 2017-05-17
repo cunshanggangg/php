e8b51542ffeab609c5825ad5d8a66a179678ceab
cunshanggangg <cunshanggangg@gmail.com>
2017-05-17 10:23:29 +0800

    How to get array from a file.

---
+++ soap/data/company.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/17
+ * Time: 10:18
+ */
+$data['company'] = array(
+    '0' => '百度',
+    '1' => '腾讯',
+    '2' => '阿里巴巴',
+    '3' => '小米',
+    '4' => '创新工场',
+    '5' => '京东'
+);
---
+++ soap/getArr.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/5/17
+ * Time: 10:20
+ */
+require_once 'data/company.php';
+echo "<pre>";
+print_r($data);
+echo "</pre>";
