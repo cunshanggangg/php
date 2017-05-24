290b733b3faf9d5fd1f9736518bdefe3a1e7668d
cunshanggang <cunshanggang@163.com>
2017-05-24 23:56:25 +0800

    More than 26 column.

--- excel/test/out1.php
+++ excel/test/out1.php
@@ -7,6 +7,7 @@
  */
 
 require_once '../Classes/PHPExcel.php';
+require_once '../Classes/PHPExcel/Cell.php';
 require_once '../Classes/PHPExcel/Writer/Excel2007.php';
 require_once '../Classes/PHPExcel/Writer/Excel5.php';
 include_once '../Classes/PHPExcel/IOFactory.php';
@@ -14,22 +15,33 @@ include_once '../Classes/PHPExcel/IOFactory.php';
 $fileName = "test_excel";
 $headArr = array("序号","姓名","年龄","性别");
 $conn = mysql_connect("localhost","root","") or die("MySQL连接不上！");
-$db = mysql_select_db("tp",$conn) or die("数据库连接失败！");
+$db = mysql_select_db("jdpt1",$conn) or die("数据库连接失败！");
 mysql_query("set names 'utf8'");
 
-$sql = "select * from tp_student";
+$sql = "select * from shopnc_order_import";
 $result = mysql_query($sql) or die(mysql_error());
 $student = array();
 $i = 0;
-while($row = mysql_fetch_array($result)) {
-    $student[$i]['id'] = $row['id'];
-    $student[$i]['name'] = $row['name'];
-    $student[$i]['age'] = $row['age'];
-    $student[$i]['sex'] = ($row['sex'] == '0') ? '男' : '女';
-    $i++;
-}
+$row_keys = mysql_fetch_assoc($result);
+$k26 = array_keys($row_keys);
+$all_keys = array_splice($k26,0,30);
+$student = array();
+$i = 0;
+    while($row = mysql_fetch_array($result)) {
+//        foreach($row_keys as $value) {
+//        $student[$i][$value] = $row[$value];
+//        }
+        for($j=0;$j<count($all_keys);$j++) {
+            $student[$i][$all_keys[$j]] = $row[$all_keys[$j]];
+        }
+        $i++;
+    }
+
+//echo "<pre>";
+//print_r($student);
+//echo "</pre>";
 //$data = array(array(1,2),array(1,3),array(5,7));
-getExcel($fileName,$headArr,$student);
+getExcel($fileName,$all_keys,$student);
 
 
 function getExcel($fileName,$headArr,$data)
@@ -53,25 +65,33 @@ function getExcel($fileName,$headArr,$data)
 //    //水平居中===垂直居中
 //    $objPHPExcel->setActiveSheetIndex(0)->getStyle('A1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 //    $objPHPExcel->setActiveSheetIndex(0)->getStyle('A1')->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER);
-    $objSheet = $objPHPExcel->getActiveSheet();
-    $objSheet->getDefaultStyle()->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER)
-        ->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+//    $objSheet = $objPHPExcel->getActiveSheet();
+//    $objSheet->getDefaultStyle()->getAlignment()->setVertical(PHPExcel_Style_Alignment::VERTICAL_CENTER)
+//        ->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
     //设置水平垂直居中
-    $objSheet->getDefaultStyle()->getFont()->setName("微软雅黑")->setSize(12); //设置默认字体大小
-    $objSheet->getStyle("A1:D1")->getFont()->setSize(16)->setBold(false); //标题字体
+//    $objSheet->getDefaultStyle()->getFont()->setName("微软雅黑")->setSize(12); //设置默认字体大小
+//    $objSheet->getStyle("A1:D1")->getFont()->setSize(16)->setBold(false); //标题字体
 //    $objSheet->getStyle('A1:D1')->getFill()->setFillType(PHPExcel_Style_Fill::FILL_SOLID)
 //        ->getStartColor()->setARGB('0000000'); //设置标题背景颜色
 //    $objSheet->getStyle("A1:F1")->applyFromArray(getBorderStyle("#66FF99")); //设置标题边框
+    //设置表头 超过26列
+    $key = 0;
+    foreach($headArr as $v){
+        //注意，不能少了。将列数字转换为字母\
+        $colum = PHPExcel_Cell::stringFromColumnIndex($key);
+        PHPExcel_Cell::columnIndexFromString('AA');
+        $objPHPExcel->setActiveSheetIndex(0)->setCellValue($colum . '1', $v);
+        $key += 1;
+    }
+    /*
     //设置表头
     $key = ord("A");
     foreach ($headArr as $v) {
         $colum = chr($key);
-        //设置font
-        $objPHPExcel->setActiveSheetIndex(0)->getStyle($key)->getFont()->setSize(24);
         $objPHPExcel->setActiveSheetIndex(0)->setCellValue($colum . '1', $v);
         $key += 1;
     }
-
+    */
     $column = 2;
     $objActSheet = $objPHPExcel->getActiveSheet();
     foreach ($data as $key => $rows) { //行写入
--- excel/test/result.php
+++ excel/test/result.php
@@ -6,13 +6,28 @@
  * Time: 17:33
  */
 $conn = mysql_connect("localhost","root","") or die("MySQL连接不上！");
-$db = mysql_select_db("tp",$conn) or die("数据库连接失败！");
+$db = mysql_select_db("jdpt1",$conn) or die("数据库连接失败！");
 mysql_query("set names 'utf8'");
 
-$sql = "select * from tp_student";
+$sql = "select * from shopnc_order_import";
 $result = mysql_query($sql) or die(mysql_error());
 $student = array();
 $i = 0;
+$row = mysql_fetch_assoc($result);
+//echo "<pre>";
+//print_r(array_keys($row));
+//echo "</pre>";
+$res = array_keys($row);
+//$k26 = array_keys($row_keys);
+$all_keys = array_splice($res,0,25);
+echo "<pre>";
+print_r($all_keys);
+echo "</pre>";exit;
+foreach($res as $value) {
+    echo $value;
+    echo "<hr>";
+}
+exit;
 while($row = mysql_fetch_array($result)) {
     $student[$i]['id'] = $row['id'];
     $student[$i]['name'] = $row['name'];
