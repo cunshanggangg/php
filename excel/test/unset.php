f0d07bb05f417c36d696c2975ef83969435f8fea
cunshanggangg <cunshanggangg@gmail.com>
2017-05-26 11:57:11 +0800

    Destroy variable using unset and array_filter.

--- excel/test/unset.php
+++ excel/test/unset.php
@@ -10,15 +10,26 @@ $arr = array(
     array("0" => "liNa","1" => "dingNing","2" => "fuYuanHui","3" => "")
 );
 
+/* 使用 unset
 foreach($arr as $key1=>$v) {
     foreach($v as $key2=>$v2) {
         if(!$v2) {
             unset($arr[$key1][$key2]);
         }
-//        array_filter($arr[$key1]);
     }
-//   $res[] =  array_filter($v);
 }
 echo "<pre>";
-print_r($arr);
+print_r($res);
 echo "</pre>";
+*/
+
+/* 使用 array_filter()
+$res = array();
+foreach($arr as $key1 => $v) {
+    $res[] = array_filter($v);
+}
+
+echo "<pre>";
+print_r($res);
+echo "</pre>";
+*/
