8e1d35f79d91732e8c545e6e5d368457dc1bcb5f
cunshanggangg <cunshanggangg@gmail.com>
2017-06-09 16:54:45 +0800

    Update address.

--- regularExp/getAddr/insert.php
+++ regularExp/getAddr/insert.php
@@ -6,6 +6,7 @@
  * Time: 15:31
  */
 require_once 'class/medoo.php';
+//获得数组中相同的元素
 function repeat($array) {
     $len = count ( $array );
     $repeat_arr = array();
@@ -19,14 +20,67 @@ function repeat($array) {
     }
     return $repeat_arr;
 }
-$str = file_get_contents("data/Japanese.txt");
+//$str = file_get_contents("data/Japanese.txt");//日本
+//$str = file_get_contents("data/USA.txt");//美国
+//$str = file_get_contents("data/Malaysian.txt");//马来西亚
+//$str = file_get_contents("data/Korean.txt");//韩国
+//$str = file_get_contents("data/Australian.txt");//澳大利亚
+//$str = file_get_contents("data/Indian.txt");//印度
+//$str = file_get_contents("data/Russian.txt");//俄罗斯
+//$str = file_get_contents("data/Canada.txt");//加拿大
+//$str = file_get_contents("data/UK.txt");//英国
+//$str = file_get_contents("data/Singaporean.txt");//新加坡
+//$str = file_get_contents("data/French.txt");//法国
+//$str = file_get_contents("data/Vietnamese.txt");//越南
+//$str = file_get_contents("data/Filipino.txt");//菲律宾
+//$str = file_get_contents("data/German.txt");//德国
+//$str = file_get_contents("data/Bengalese.txt");//孟加拉国
+//$str = file_get_contents("data/Indonesian.txt");//印度尼西亚
+//$str = file_get_contents("data/NewZealander.txt");//新西兰
+//$str = file_get_contents("data/Italian.txt");//意大利
+//$str = file_get_contents("data/Nepali.txt");//尼泊尔
+//$str = file_get_contents("data/Spanish.txt");//西班牙
+//$str = file_get_contents("data/Thai.txt");//泰国
+//$str = file_get_contents("data/Brazilian.txt");//巴西
+//$str = file_get_contents("data/Iranian.txt");//伊朗
+$str = file_get_contents("data/Burmese.txt");//缅甸
+
+
+
+
+
+
 //echo "<pre>";
 //echo $str;
 $preg = "/(.*)\r\n/";
 preg_match_all($preg,$str,$result);
 //echo "<pre>";
 //print_r($result[1]);
-$res = array_slice($result[1],0,439);//获取393条
+//$res = array_slice($result[1],0,439);//日本：获取393条
+//$res = array_slice($result[1],0,300);//美国：获取300条
+//$res = array_slice($result[1],0,212);//马来西亚：获取186条
+//$res = array_slice($result[1],0,149);//韩国：获取139条
+//$res = array_slice($result[1],0,137);//澳大利亚：获取137条
+//$res = array_slice($result[1],0,110);//印度：获取109条
+//$res = array_slice($result[1],0,75);//俄罗斯：获取74条
+//$res = array_slice($result[1],0,70);//加拿大：获取70条
+//$res = array_slice($result[1],0,69);//英国：获取69条
+//$res = array_slice($result[1],0,56);//新加坡：获取54条
+//$res = array_slice($result[1],0,51);//法国：获取51条
+//$res = array_slice($result[1],0,50);//越南：获取50条
+//$res = array_slice($result[1],0,50);//菲律宾：获取50条
+//$res = array_slice($result[1],0,48);//德国：获取48条
+//$res = array_slice($result[1],0,47);//孟加拉国：获取46条
+//$res = array_slice($result[1],0,42);//印度尼西亚：获取42条
+//$res = array_slice($result[1],0,40);//新西兰：获取40条
+//$res = array_slice($result[1],0,37);//意大利：获取37条
+//$res = array_slice($result[1],0,31);//尼泊尔：获取28条
+//$res = array_slice($result[1],0,27);//尼泊尔：获取27条
+//$res = array_slice($result[1],0,24);//泰国：获取24条
+//$res = array_slice($result[1],0,24);//巴西：获取24条
+//$res = array_slice($result[1],0,19);//伊朗：获取19条
+$res = array_slice($result[1],0,16);//缅甸：获取16条
+
 //echo "<pre>";
 //print_r($res);
 //去掉重复的值
@@ -35,7 +89,7 @@ $res1 = array_unique($res);
 //print_r(array_values($res1));
 $res2 = array_values($res1);
 //echo "<pre>";
-//print_r($res);
+//print_r($res2);
 //if(count($res) != count(array_unique($res))) {
 //    echo "数组有重复";
 //    echo count($res);
@@ -67,14 +121,38 @@ $database = new medoo([
     'option'=> [PDO :: ATTR_CASE => PDO :: CASE_NATURAL]
 ]);
 
-//$japan = $database->debug()->select("member","*",array("country" => "日本"));
-$japan = $database->select("member",array("user_id"),array("country" => "日本"));
+//$country = $database->debug()->select("member","*",array("country" => "日本"));
+//$country = $database->select("member",array("user_id"),array("country" => "美国"));
+//$country = $database->select("member",array("user_id"),array("country" => "马来西亚"));
+//$country = $database->select("member",array("user_id"),array("country" => "韩国"));
+//$country = $database->select("member",array("user_id"),array("country" => "澳大利亚"));
+//$country = $database->select("member",array("user_id"),array("country" => "印度"));
+//$country = $database->select("member",array("user_id"),array("country" => "俄罗斯"));
+//$country = $database->select("member",array("user_id"),array("country" => "加拿大"));
+//$country = $database->select("member",array("user_id"),array("country" => "英国"));
+//$country = $database->select("member",array("user_id"),array("country" => "新加坡"));
+//$country = $database->select("member",array("user_id"),array("country" => "法国"));
+//$country = $database->select("member",array("user_id"),array("country" => "越南"));
+//$country = $database->select("member",array("user_id"),array("country" => "菲律宾"));
+//$country = $database->select("member",array("user_id"),array("country" => "德国"));
+//$country = $database->select("member",array("user_id"),array("country" => "孟加拉国"));
+//$country = $database->select("member",array("user_id"),array("country" => "印度尼西亚"));
+//$country = $database->select("member",array("user_id"),array("country" => "新西兰"));
+//$country = $database->select("member",array("user_id"),array("country" => "意大利"));
+//$country = $database->select("member",array("user_id"),array("country" => "尼泊尔"));
+//$country = $database->select("member",array("user_id"),array("country" => "西班牙"));
+//$country = $database->select("member",array("user_id"),array("country" => "泰国"));
+//$country = $database->select("member",array("user_id"),array("country" => "巴西"));
+//$country = $database->select("member",array("user_id"),array("country" => "伊朗"));
+$country = $database->select("member",array("user_id"),array("country" => "缅甸"));
+
+
 //echo "<pre>";
 //print_r($japan);
 //foreach($japan as $key => $value) {
 //    echo $value['user_id'];
 //    echo "<hr>";
 //}
-foreach($japan as $key => $value) {
+foreach($country as $key => $value) {
        $database->update("member",array("address" => $res2[$key]),array("user_id" => $value['user_id']));
 }
