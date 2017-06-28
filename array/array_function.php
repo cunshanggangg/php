7ec2f1f2496815055752389ca3fed69f5877af89
cunshanggangg <cunshanggangg@gmail.com>
2017-06-28 10:13:15 +0800

    About array functions.

---
+++ array/array_function.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/6/28
+ * Time: 10:01
+ */
+#队列，栈
+/*
+ * 1.array_push()
+ * 2.array_pop()
+ * 3.array_shift()
+ * 4.array_unshift()
+ *
+ * */
+
+#合并
+/*
+ * 5.array_combine()
+ * 6.array_merge()
+ *
+ * */
+
+#切割
+/*
+ * 7.array_splice()
+ * 8.array_slice()
+ * 9.array_filter()
+ * 10.array_chunk()
+ * 11.array_map() //回调函数
+ *
+ * */
+
+#键，值
+/*
+ * 12.array_values()
+ * 13.array_keys()
+ * 14.array_unique()
+ * 15.array_sum()
+ * 16.array_flip()
+ *
+ * */
+
+#反转
+/*
+ * 17.array_reserve()
+ *
+ * */
+
+#交、差集
+/*
+ * array_diff()
+ * array_diff_assoc()
+ * array_diff_uassoc()
+ * array_intersect()
+ * array_intersect_key()
+ *
+ * */
+
+#其他
+/*
+ * array_search()
+ * array_rand()
+ * is_array()
+ * in_array()
+ * 
+ * */
