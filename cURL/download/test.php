a5d3f935486ee63e7e75e9d5247be9c42ed917f7
cunshanggangg <cunshanggangg@gmail.com>
2017-07-07 17:42:33 +0800

    Using cURL to download file.

---
+++ cURL/download/test.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/7/7
+ * Time: 14:48
+ */
+$ch=curl_init();
+$url="http://localhost/php/cURL/download/data/test.txt";
+  curl_setopt($ch,CURLOPT_URL, $url);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+  $content=curl_exec($ch);
+  if(curl_errno($ch)){
+      echo curl_error($ch);
+      curl_close($ch);
+  }else{
+      curl_close($ch);
+      //提取文件名和文件类型
+      $nameArr=explode('/',$url);
+  $last_index=count($nameArr)-1;
+  $file_name=$nameArr[$last_index];
+  $typeArr=explode('.',$url);
+  $last_index=count($typeArr)-1;
+  $file_type=$typeArr[$last_index];
+  //获得文件大小
+  $file_size=strlen($content);
+  //通知浏览器下载文件
+  header("Content-type: application/$file_type");
+  header('Content-Disposition: attachment; filename="'.$file_name.'"');
+  header("Content-Length:".$file_size);
+  exit($content); //输出数据流
+  }
