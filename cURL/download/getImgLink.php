2a81ff55b0c45a8dd4018942d3479f58b3feab9d
cunshanggangg <cunshanggangg@gmail.com>
2017-07-18 18:07:56 +0800

    Using curl to grab the image.

--- cURL/download/getImgLink.php
+++ cURL/download/getImgLink.php
@@ -6,11 +6,14 @@
  * Time: 16:11
  */
 
-$str = file_get_contents('http://www.sd020.com/shop/item-105231.html');
-//echo $str;
-preg_match_all("<img src=\"(.*?)\" alt=\"image\">",$str,$result);
-echo "<pre>";
-print_r($result[1]);
+//$str = file_get_contents('http://www.sd020.com/shop/item-105231.html');
+////echo $str;
+//preg_match_all("<img src=\"(.*?)\" alt=\"image\">",$str,$result);
+//echo "<pre>";
+//print_r($result[1]);
+//$r = explode('/',$result[1][0]);
+//echo "<pre>";
+//print_r(end($r));
 
 
 $str1 = file_get_contents('http://www.sd020.com/shop/index.php?act=search&op=index&keyword=&is_collect=1');
@@ -30,19 +33,24 @@ foreach($fileName as $k => $v) {
 
 //echo "<pre>";
 //print_r($arr);
-/*
+
 foreach($fileName as $k1 => $v1) {
     $str = file_get_contents($v1);
     preg_match_all("<img src=\"(.*?)\" alt=\"image\">",$str,$result);
     foreach($result[1] as $k2 => $v2) {
-        download_img($v2,);
+        $r = explode('/',$result[1][0]);
+        $file = end($r);
+        if(!file_exists($arr[$k1])) {
+            mkdir($arr[$k1],0777,true);
+        }
+        download_img($v2,$arr[$k1],$file);
     }
 }
 
-function download_img($url = "", $filename = "")
+function download_img($url = "", $dir = "", $filename = "")
 {
     $ch = curl_init(); //初始化一个curl句柄
-    $hd = fopen($filename,'wb'); //只写打开或新建一个二进制文件；只允许写数据
+    $hd = fopen($dir.'/'.$filename,'wb'); //只写打开或新建一个二进制文件；只允许写数据
     curl_setopt($ch,CURLOPT_URL,$url); //需要获取的 URL 地址
     curl_setopt($ch,CURLOPT_FILE,$hd); //设置成资源流的形式
     curl_setopt($ch,CURLOPT_HEADER,0); //启用时会将头文件的信息作为数据流输出。
@@ -57,4 +65,3 @@ function download_img($url = "", $filename = "")
     fclose($hd); //关闭句柄
     return true;
 }
-*/
