a3fad804d88eca99fc5260417530935d405e98ec
cunshanggangg <cunshanggangg@gmail.com>
2017-08-02 17:36:59 +0800

    Finished jsonp + jQuery + ajax + php.

--- jquery/jsonp/jspon.php
+++ jquery/jsonp/jspon.php
@@ -5,7 +5,7 @@
  * Date: 2017/8/1
  * Time: 15:16
  */
-error_reporting(0);
+//error_reporting(0);
 //header("content-type: text/javascript");
 
 if(isset($_GET['name']) && isset($_GET['callback'])) {
@@ -15,3 +15,12 @@ if(isset($_GET['name']) && isset($_GET['callback'])) {
     echo $_GET['callback']. '(' . json_encode($obj) . ');';
 //    echo 12345;
 }
+
+////服务端返回JSON数据
+//$arr=array('a'=>1,'b'=>2,'c'=>3,'d'=>4,'e'=>5);
+//$result=json_encode($arr);
+////echo $_GET['callback'].'("Hello,World!")';
+////echo $_GET['callback']."($result)";
+////动态执行回调函数
+//$callback=$_GET['callback'];
+//echo $callback."($result)";
