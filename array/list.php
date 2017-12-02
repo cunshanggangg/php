e603d923aac19a87b77b8e8bc5f4fc4c2d435d22
cunshanggang <cunshanggang@163.com>
2017-12-02 17:04:34 +0800

    How to use list().

---
+++ array/list.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/2
+ * Time: 16:57
+ */
+//给变量赋予数组中的某一个值
+$arr = array('yaoMing','liNa','sunYang');
+list($a,$b,$c) = $arr;
+
+echo "We all love ".$a.' '.$b.' '.$c;
