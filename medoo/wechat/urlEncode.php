6d340fa4ca03cc4411cf6b60e002be2c8e2566a2
cunshanggangg <cunshanggangg@gmail.com>
2018-01-30 09:46:20 +0800

    Js decodeURIComponent().

--- medoo/wechat/urlEncode.php
+++ medoo/wechat/urlEncode.php
@@ -5,7 +5,16 @@
  * Date: 2018/1/28
  * Time: 15:48
  */
-$keyword = "李连杰";
-$join_time = "2018-01-28 14:29:20";
-$contentStr = "localhost/php/medoo/wechat/show.php?keyword=".urlencode($keyword)."&time=".urlencode($join_time);
-echo $contentStr;
+//$keyword = "李连杰";
+//$join_time = "2018-01-28 14:29:20";
+//$contentStr = "localhost/php/medoo/wechat/show.php?keyword=".urlencode($keyword)."&time=".urlencode($join_time);
+//echo $contentStr;
+
+$time = date("Y-m-d H:i:s");
+echo $time;
+echo "<hr>";
+$urlEncode = urlencode($time);
+echo $urlEncode;
+echo "<pre>";
+echo "</pre>";
+
