762ae638439d56c301c2aac8ca284e14a6b57056
cunshanggangg <cunshanggangg@gmail.com>
2018-04-28 14:56:01 +0800

    Finished to find the time period.

--- time/period.php
+++ time/period.php
@@ -5,6 +5,7 @@
  * Date: 2018/4/28
  * Time: 0:01
  */
+date_default_timezone_set("PRC");
 $s = '2017-02-05';
 $e = '2017-02-21';
 
@@ -14,6 +15,12 @@ $end      = new \DateTime($e);
 $interval = \DateInterval::createFromDateString('1 day');
 $period   = new \DatePeriod($start, $interval, $end);
 
+$timeStamp = array();
 foreach ($period as $dt) {
     echo $dt->format("Y-m-d") . "<br>\n";
+    array_push($timeStamp,strtotime($dt->format("Y-m-d")));
 }
+
+echo "<pre>";
+print_r($timeStamp);
+echo "</pre>";
