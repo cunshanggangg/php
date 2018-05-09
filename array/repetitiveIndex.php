5e478744050e8479b851fe519b9607cb02b25d36
cunshanggangg <cunshanggangg@gmail.com>
2018-05-09 17:35:38 +0800

    Repetitive index in an array.

---
+++ array/repetitiveIndex.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linjiabei
+ * Date: 2018/5/9
+ * Time: 15:39
+ */
+$arr = array('yao'=>1,'yao'=>2);
+echo "<pre>";
+print_r(count($arr));
+echo "</pre>";
