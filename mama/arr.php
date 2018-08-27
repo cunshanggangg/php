18da3097a17f3d3be0dcccdc5cd18f580f6cfeaf
linjiabei <linjb@mama.cn>
2018-08-27 10:02:56 +0800

    Print mama's diary blog.

---
+++ mama/arr.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: james
+ * Date: 2018/8/24
+ * Time: 10:13
+ */
+/*
+$arr = [
+    "data" => '{"stage":["1","2","3"],"stage_id":[],"desc":["丁彦雨航1","丁彦雨航2","丁彦雨航3"],"content":["<p>你好，丁彦雨航1</p>","<p>你好，丁彦雨航2</p>","<p>你好，丁彦雨航3</p>"]}',
+    "tid" => 0,
+    "uid" => 65573436,
+    "username" => "linjiabei",
+    "title" => "丁彦雨航",
+    "icon" => "111111111",
+    "city_id" => 289,
+    "community" => 1,
+    "decorate_type" => 1,
+    "work_type" => 1,
+    "type_id" => 1,
+    "style_id" => 1,
+    "area" => 1,
+    "fee" => 1,
+    "build_start_time" => 1,
+    "designer" => 1,
+    "bid" => 0,
+    "businessman_name" => 1,
+    "mobile" => "13570473850"
+];
+*/
+/*
+$arr = [
+    "data" => '{"stage":["1","2","3"],"stage_id":["275","276","277"],"desc":["丁彦雨航更新啦1","丁彦雨航更新啦2","丁彦雨航更新啦3"],"content":["<p>你好，丁彦雨航更新1</p>","<p>你好，丁彦雨航更新2</p>","<p>你好，丁彦雨航更新3</p>"]}',
+    "id" => 257,
+    "tid" => 5176448,
+    "uid" => 65573436,
+    "username" => "linjiabei",
+    "title" => "丁彦雨航更新啦",
+    "icon" => "111111111",
+    "city_id" => 289,
+    "community" => 1,
+    "decorate_type" => 1,
+    "work_type" => 1,
+    "type_id" => 1,
+    "style_id" => 1,
+    "area" => 1,
+    "fee" => 1,
+    "build_start_time" => 1,
+    "designer" => 1,
+    "bid" => 0,
+    "businessman_name" => 1,
+    "mobile" => "13570473850"
+];
+*/
+$arr = [
+    "data" => '{"stage":["1","2","3","4","5"],"stage_id":["275","276","277","",""],"desc":["丁彦雨航更新啦1","丁彦雨航更新啦2","丁彦雨航更新啦3","丁彦雨航添加阶段4","丁彦雨航添加阶段5"],"content":["<p>你好，丁彦雨航更新1</p>","<p>你好，丁彦雨航更新2</p>","<p>你好，丁彦雨航更新3</p>","新增内容4","新增内容5"]}',
+    "id" => 257,
+    "tid" => 5176448,
+    "uid" => 65573436,
+    "username" => "linjiabei",
+    "title" => "丁彦雨航新增啦",
+    "icon" => "111111111",
+    "city_id" => 289,
+    "community" => 1,
+    "decorate_type" => 1,
+    "work_type" => 1,
+    "type_id" => 1,
+    "style_id" => 1,
+    "area" => 1,
+    "fee" => 1,
+    "build_start_time" => 1,
+    "designer" => 1,
+    "bid" => 0,
+    "businessman_name" => 1,
+    "mobile" => "13570473850"
+];
+echo "<pre>";
+print_r($arr);
+echo "</pre>";
